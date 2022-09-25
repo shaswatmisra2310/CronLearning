2bbf12da1d3fc50e8f996dd4560fcb01502a4061
Shaswat Misra <shaswatmisra2310@gmail.com>
2022-09-25 20:01:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿var timer = new CronTimer("0/10 * * * *", "Europe/Amsterdam", includingSeconds: false);
+timer.OnOccurence += (s, ea) => Console.Out.WriteLineAsync(ea + " - " + DateTime.Now);
+timer.Start();
